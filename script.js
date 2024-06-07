d0a75140982ae3a77974ec523e6c77a202ed49ad
Anmol Gupta <anmolg2411@gmail.com>
2024-06-07 18:33:47 +0530

    final 2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const navbarRight = document.getElementById('navbar-right');
     const subNavbarToggle = document.getElementById('sub-navbar-toggle');
     const subNavbarLinks = document.getElementById('sub-navbar-links');
+
   
     navbarToggle.addEventListener('click', () => {
       navbarRight.classList.toggle('active');
@@ -26,3 +27,23 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
+
+
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change image every 3 seconds
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showSlides();
+});
